24cf396360dfb02d29529b2886620fd903cca9d3
Drew McMillan <drew.mcmillan@bbc.co.uk>
2019-05-18 18:04:39 +0100

    :rocket: Extract blog list into component

---
+++ src/components/BlogList.jsx
@@ -0,0 +1,34 @@
+import React, { Fragment } from 'react';
+import styled from 'styled-components';
+import { Link } from 'gatsby';
+import get from '../utils/deepGet';
+import { rhythm } from '../utils/typography';
+
+const Heading = styled.h3`
+  margin-bottom: ${rhythm(1 / 4)};
+`;
+
+const Bio = ({ posts, title }) => (
+  <Fragment>
+    <h2>{title}</h2>
+    {posts.map(({ node }) => {
+      const title =
+        get(['frontmatter', 'title'], node) ||
+        get(['frontmatter', 'path'], node);
+
+      return (
+        <div key={node.frontmatter.path}>
+          <Heading>
+            <Link style={{ boxShadow: 'none' }} to={node.frontmatter.path}>
+              {title}
+            </Link>
+          </Heading>
+          <small>{node.frontmatter.date}</small>
+          <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+        </div>
+      );
+    })}
+  </Fragment>
+);
+
+export default Bio;
--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -1,16 +1,11 @@
 /* eslint-disable react/no-danger */
 import React from 'react';
-import styled from 'styled-components';
-import { Link, graphql } from 'gatsby';
+import { graphql } from 'gatsby';
 import Helmet from 'react-helmet';
 import get from '../../utils/deepGet';
 
+import BlogList from '../../components/BlogList';
 import Layout from '../../components/Layout';
-import { rhythm } from '../../utils/typography';
-
-const Heading = styled.h3`
-  margin-bottom: ${rhythm(1 / 4)};
-`;
 
 const BlogIndex = ({ data, location }) => {
   const siteTitle = get(['site', 'siteMetadata', 'title'], data);
@@ -24,24 +19,7 @@ const BlogIndex = ({ data, location }) => {
         meta={[{ name: 'description', content: siteDescription }]}
         title={`Blog | ${siteTitle}`}
       />
-      <h2>Blog</h2>
-      {posts.map(({ node }) => {
-        const title =
-          get(['frontmatter', 'title'], node) ||
-          get(['frontmatter', 'path'], node);
-
-        return (
-          <div key={node.frontmatter.path}>
-            <Heading>
-              <Link style={{ boxShadow: 'none' }} to={node.frontmatter.path}>
-                {title}
-              </Link>
-            </Heading>
-            <small>{node.frontmatter.date}</small>
-            <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
-          </div>
-        );
-      })}
+      <BlogList posts={posts} title="Blog" />
     </Layout>
   );
 };
