2438e6c052e1f5adf7396091619237bf71b87cbd
Drew McMillan <drew.mcmillan@bbc.co.uk>
2019-05-19 16:23:02 +0100

    :rocket: Deploy to travis

---
+++ scripts/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e # Exit with nonzero exit code if anything fails
+
+SOURCE_BRANCH="deploy"
+
+# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
+if [ "$TRAVIS_PULL_REQUEST" == "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo "Skipping deploy;"
+    exit 0
+fi
+
+git remote set-url origin "https://${GITHUB_TOKEN}@github.com/dr3/drew.mx.git"
+git config user.email "foo@bar.com"
+git config user.name "Travis Ci"
+npm run deploy
