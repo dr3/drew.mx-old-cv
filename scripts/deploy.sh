0ff81b76aeaba586e465d057d1ddde063cae2197
Drew McMillan <drew.mcmillan@bbc.co.uk>
2019-05-19 16:46:06 +0100

    :rocket: Change for merge

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-SOURCE_BRANCH="deploy"
+SOURCE_BRANCH="latest"
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-# if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-#     echo "Skipping deploy; just doing a build."
-#     exit 0
-# fi
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo "Skipping deploy."
+    exit 0
+fi
 
 git remote set-url origin "https://${GITHUB_TOKEN}@github.com/dr3/drew.mx.git"
 git config user.email "foo@bar.com"
