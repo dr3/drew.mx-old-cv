5f4cde50fd9bf29b78c29bc955b7ab8175110b56
Drew McMillan <drew.mcmillan@bbc.co.uk>
2019-05-20 00:27:39 +0100

    :rocket: Move to drew.mx

--- data/config/site.js
+++ data/config/site.js
@@ -1,9 +1,9 @@
 module.exports = {
-  title: '🙈 Drew McMillan',
+  title: 'Drew McMillan - Software Engineer',
   author: 'Drew McMillan',
   description:
     'Personal website of Drew McMillan, a software engineer in London, UK. Drew is currently working at BBC News on a brand new React/Node stack and has a strong interest in all things frontend web.',
-  siteUrl: 'https://new.drew.mx',
+  siteUrl: 'https://drew.mx',
   githubUrl: 'https://github.com/dr3',
   repoUrl: 'https://github.com/dr3/drew.mx',
 };
