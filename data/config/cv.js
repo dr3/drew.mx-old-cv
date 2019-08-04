8eafe274e59d9c1e0fa319508aedbe9861cca919
Drew McMillan <drew.mcmillan@bbc.co.uk>
2019-08-04 10:42:28 +0100

    :pencil: Update and fit

--- data/config/cv.js
+++ data/config/cv.js
@@ -13,9 +13,9 @@ module.exports = {
       subsections: [
         {
           body: [
-            'Currently working to scale a React application across BBC World Service, I am very a enthusiastic developer with hands on experience working on websites used by millions of users a month. I have experience with the full process of developing robust web applications from analysis and design though to development, testing and deploying.',
-            'Achieving a 1st in computer science in 2018, I have a large number of both technical and personal skills. I am very adaptable to new infrastructures and languages, quickly being able to develop new features and improve existing code. I have a wide range of personal skills such as time keeping, team work to ensure that I can develop effective software and write quality code.',
-            'Over the next few years I hope to continue to build both my technical and personal skills, mentoring others to become better developers themselves while continuing to force myself a strong career in software engineering. I hope that what you read will put me in good stride to become an excellent member, and senior software engineer, of your team.'
+            'Currently working to scale a React application across BBC World Service, I am very a enthusiastic developer working on websites used by millions of users a month. I have experience with the full process of developing robust web applications from analysis and design though to development, testing and deploying.',
+            'Achieving a 1st in Computer Science in 2018, I have a large number of both technical and personal skills. I am very adaptable to new infrastructures and languages, quickly being able to develop new features and improve existing code. I have a wide range of personal skills such as time keeping, team work to ensure that I can develop effective software and write quality code.',
+            'Over the next few years I hope to continue to build both my technical and personal skills, mentoring others to become better developers themselves while continuing to force myself a strong career in software engineering. I hope that what you read will put me in good stride to become an excellent member, and senior software engineer, of your team.',
           ],
         },
       ],
@@ -26,32 +26,31 @@ module.exports = {
         {
           label: 'Creating effective software',
           body: [
-            'Analysing product requirements I can lead decision making and development to meet the requirements in a timely and suitable manor. Understanding available technologies quickly I can lead others, weighing all opinions, to create effective software that is performant, scalable and accessible',
+            'Analysing product requirements I can lead decision making and development to meet the requirements in a timely and suitable manor. Understanding available technologies quickly, I can lead others and weigh all opinions to create effective software that is performant, scalable and accessible.',
           ],
         },
         {
           label: 'Adapting quickly',
           body: [
-            'When working in an agile team you can often move to a new software system when a new epic is begun. I am very strong at quickly understanding the new and unknown infrastructure, and developing within it. I strive to develop my skills as fast as possible and frequently explore new technologies to explore how they can be used to help serve our audience.',
+            'When working in an agile team you can often move to a new software system when a new epic is begun. I am very strong at quickly understanding the new and unknown infrastructure, and developing within it. I strive to develop my skills as fast as possible and frequently explore new technologies to understand how they can be used to help better serve our audience.',
           ],
         },
         {
           label: 'Code analysis and review',
           body: [
-            'Being a code owner of our primary applications, I have repeatedly shown I can give constructive feedback on others’ code, foresee future issues that may arise and rapidly come up with solutions to fix them. Dealing with legacy code bases I can analyse which areas are suitable to reuse and weigh the cost of redevelopment to ensure we deliver the best product possible',
-            'Code reviews can be an important part of an engineers development. With a strong focus on helping others grow as a developer, I aim to provide good feedback during code reviews so that others can not only correct their code, but also learn from the process as much as possible.',
+            'Being a code owner of our primary applications, I have repeatedly shown I can give constructive feedback on others’ code to help them improve, as well as foresee future issues that may arise and rapidly come up with solutions to fix them. Dealing with legacy code bases I can analyse which areas are suitable to reuse and weigh the cost of redevelopment to ensure we deliver the best product possible.',
           ],
         },
         {
           label: 'Teamwork and Personal Skills',
           body: [
-            'While I am currently a mid level developer, I have shown my ability to lead both team and application development. Having recently develivered successfully with a relatively inexperienced team, guiding their development and teaching where needed to grow both the team and myself.',
+            'While I am currently a mid level developer, I have shown my ability to lead both team and application development. Having recently delivered successfully with a relatively inexperienced team, guiding their development and teaching where needed to grow both the team and myself.',
           ],
         },
         {
           label: 'Applying best practices',
           body: [
-            'Constantly learning best practices, I am able to best solve problems using suitable technology to deliver user value. Using CD and versioning to deliver faster, ensuring best practices are followed in HTML semantics, React Js and caching are just a few of the many ways I have shown my ability to understand and apply technologies when suitable',
+            'Constantly learning best practices, I am able to best solve problems using suitable technology to deliver user value. Using CD and versioning to deliver faster, ensuring best practices are followed in HTML semantics, React JS and caching are just a few of the many ways I have shown my ability to understand and apply technologies when suitable.',
           ],
         },
       ],
@@ -65,7 +64,7 @@ module.exports = {
           body: [
             'Working with the latest in full stack Javascript to build the future of BBC article pages in ReactJS. Current work can be found at https://github.com/bbc/simorgh and https://github.com/bbc/psammead which are entirely opensource.',
             'Key Project -',
-            'Simorgh - Originally on the core Articles team, I have become a central member of the Simorgh project. At its core it is an Isomorphic React application that serves articles, and now frontpages to users both in the UK and around the world. Delivering twice in just a few months, I have shown throughout this project not only strong technical ability but also leadership and the ability to mentor others. The second deliverable, BBC World Service Frontpages were delivered with a team inexperienced on the codebase. Through regular code reviews and many workshop sessions I helped guide us to delivering very rapidly.',
+            "Simorgh - Originally on the core Articles team, I have become a central member of the Simorgh project. At it's core it is an Isomorphic React application that serves articles and frontpages to users both in the UK and around the world. Delivering twice in just a handful of months, I have shown throughout this project not only strong technical ability but also leadership and the ability to mentor others. The second deliverable, BBC World Service Frontpages, were delivered with a team inexperienced on the codebase and through regular code reviews and many workshop sessions I helped guide us to delivering very rapidly.",
           ],
         },
         {
--- src/components/CV/Wrapper.jsx
+++ src/components/CV/Wrapper.jsx
@@ -4,7 +4,7 @@ import { fontFamily } from './utils';
 const Wrapper = styled.div`
   max-width: 57rem;
   margin: 0 auto;
-  line-height: 1.35rem;
+  line-height: 1.25rem;
   ${fontFamily};
   color: black;
   padding: 0 1rem;
