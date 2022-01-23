c4f6708650eb22537780316cd02bfa65cf6b066f
Drew McMillan <drew@mcmill.co.uk>
2022-01-23 12:19:35 +0000

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-71ba7e19985521ba2e7e.js"
+    "url": "webpack-runtime-1a8505b2a6c1e95ae98a.js"
   },
   {
     "url": "framework-beda7bde9d7dca0755ce.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "a61da963109d96e25b3cb7034d3b8816"
+    "revision": "7d8895754cbd53a331551b0780878002"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-ad6431e4664bcf916d19.js"
